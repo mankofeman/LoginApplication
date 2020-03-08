d270f67e3099608d6e7c066ea397b01f95eeb7b0
Aleksandr <kuznetsov.a.a@inbox.ru>
2020-03-08 19:40:19 +0500

    Initial Commit

---
+++ LoginApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApplication
+//
+//  Created by Alex Smith on 3/8/20.
+//  Copyright © 2020 Alex Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
